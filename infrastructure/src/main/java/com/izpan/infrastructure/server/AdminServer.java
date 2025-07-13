3538dbe8e86e7de656305a60418b9bb6bffb4e39
chenyc55879 <chenyc55879@hundsun.com>
2025-07-13 14:10:40 +0800

    rebuild AdminRequestBody

--- infrastructure/src/main/java/com/izpan/infrastructure/server/AdminServer.java
+++ infrastructure/src/main/java/com/izpan/infrastructure/server/AdminServer.java
@@ -1,6 +1,7 @@
 package com.izpan.infrastructure.server;
 
 import com.alipay.remoting.ConnectionEventType;
+import com.alipay.remoting.exception.RemotingException;
 import com.alipay.remoting.rpc.RpcServer;
 import com.izpan.infrastructure.server.processor.AdminServerUserProcessor;
 import com.izpan.infrastructure.server.processor.ServerConnectProcessor;
@@ -20,5 +21,10 @@ public class AdminServer {
         this.server.startup();
         log.info("DynamicTp admin server started, port: {}", port);
     }
+
+//    public Object requestToClient(AdminRequestTypeEnum requestType, Object body) throws RemotingException, InterruptedException {
+//        AdminRequestBody requestBody = new AdminRequestBody(requestType, body);
+//        return server.invokeSync(server.getConnectionManager().get(), requestBody, 30000);
+//    }
 
 }
